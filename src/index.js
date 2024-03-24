1fe826182987d5e080f7e5240c2a60cf79aa7f47
reyleal27 <reyvincentleal@gmail.com>
2024-03-24 18:42:35 +0800

    css

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-import './styles.css';
+import './index.css';
 import App from 'components/App';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
